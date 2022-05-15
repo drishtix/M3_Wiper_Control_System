a662b8ca7c8a0ae2fc4abf876da585ccf9eeca60
drishtix <101036090+drishtix@users.noreply.github.com>
2022-05-15 21:21:48 +0530

    Add files via upload

---
+++ 3_Implementation/inc/wiper_system_1.h
@@ -0,0 +1,28 @@
+/**
+ * @file wiper_system_1.h
+ * @author Drishti Shrivastava
+ * @brief Header file for wiper_system
+ *
+ */
+#ifndef WIPER_SYSTEM_1_H_INCLUDED
+#define WIPER_SYSTEM_1_H_INCLUDED
+
+void turnoff_wiper(void);
+
+void turnon_wiper(void);
+
+void mainkey_off(void);
+
+void mainkey_on(void);
+
+void Delay_ms(void);
+
+void motion_direction_clockwise(void);
+
+void motion_direction_anticlockwise(void);
+
+void Reset_Handler(void);
+
+
+
+#endif 
